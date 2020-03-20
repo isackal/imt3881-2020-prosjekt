d5bbf04fc5f3e9c5d29518606ad5187a4e92eca4
aslakto <aslakto@stud.ntnu.no>
2020-03-20 16:24:32 +0100

    Implementert demosaic.py med bugs Tror bugs er i inpaint.py men kan ikke forklare hvorfor green funker men red og blue funker ikke. Feilene er heller ikke liknende

---
+++ src/modules/demosaic.py
@@ -0,0 +1,57 @@
+import numpy as np
+import modifiers as md
+
+from inpaint import inpaint
+
+#   Brukt for testfunksjon. Slett ved endelig release
+import imageio
+import matplotlib.pyplot as plt
+
+
+def demosaic(red, green, blue):
+    redMask = ~red.astype(bool)
+    greenMask = ~green.astype(bool)
+    blueMask = ~blue.astype(bool)
+    img = np.zeros((red.shape[0], red.shape[1], 3))
+
+    new_red = inpaint(red, 10, redMask)
+    new_green = inpaint(green, 10, greenMask)
+    new_blue = inpaint(blue, 10, blueMask)
+
+    # BUG: Rød og Blå lager tilfeldige kopier av ukjent grunn
+    # Lager kopier av gresset og andre elementer den ikke burde legge til
+    img[:, :, 0] = new_red
+    img[:, :, 1] = new_green
+    # Lager kopier og speielde bilder av vaskebjørnen
+    img[:, :, 2] = new_blue
+    return img.astype(np.uint8)
+
+
+class Demosaic(md.Modifier):
+    #   read usage in ../modifiers.py
+    def __init__(self):
+        super().__init__()
+        self.name = "demosaic"
+        self.function = demosaic
+        self.params = [
+            ("red", np.ndarray, None),
+            ("green", np.ndarray, None),
+            ("blue", np.ndarray, None)
+        ]
+        self.initDefaultValues()
+
+
+#   Testfunksjon. Slett ved endelig release
+if __name__ == "__main__":
+    img = imageio.imread('../../face.png')
+    red = np.zeros(img.shape[:2])
+    blue = np.zeros(img.shape[:2])
+    green = np.zeros(img.shape[:2])
+    red[::2, ::2] = img[::2, ::2, 0]
+    green[1::2, ::2] = img[1::2, ::2, 1]
+    green[::2, 1::2] = img[::2, 1::2, 1]
+    blue[1::2, 1::2] = img[1::2, 1::2, 2]
+
+    new_img = demosaic(red, green, blue)
+    plt.imshow(new_img)
+    plt.show()
