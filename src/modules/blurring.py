2d0c9eb1f6a5c078af94d7e481c76ebc0df02264
aslakto <aslakto@stud.ntnu.no>
2020-03-22 15:23:03 +0100

    Lagt til kommentarer til blurring funksjonen

--- src/modules/blurring.py
+++ src/modules/blurring.py
@@ -26,11 +26,14 @@ def blurring(img, n, alpha, mask):
 
     new_img = img.astype(float) / 255
     centerMask = mask.astype(bool)
+
+    # Ensure blurring is not attempted directly on the boundary
     centerMask[0:2, :] = False
     centerMask[:, 0:2] = False
     centerMask[-2:, :] = False
     centerMask[:, -2:] = False
 
+    # Create diffrent views of blurring region for laplace
     topMask = np.roll(centerMask, -1, axis=0)
     botMask = np.roll(centerMask, 1, axis=0)
     leftMask = np.roll(centerMask, -1, axis=0)
