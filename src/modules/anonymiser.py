acfe72acb7754699db20f1365219714962211838
aslakto <aslakto@stud.ntnu.no>
2020-03-26 14:32:40 +0100

    Implementert grunnleggende annsiktsgennskjenning

--- src/modules/anonymiser.py
+++ src/modules/anonymiser.py
@@ -1,9 +1,9 @@
 import numpy as np
 import modifiers as md
-import cv2 as cv
+import cv2
 
 from modules.blurring import blurring
-
+from modules.colortogray import color_to_gray
 #   Brukt for testfunksjon. Slett ved endelig release
 import imageio
 import matplotlib.pyplot as plt
@@ -11,11 +11,24 @@ import matplotlib.pyplot as plt
 
 def anonymisering(img):
     mask = imageio.imread('../../People_binary_mask.jpg').astype(bool)
-    """
-    mask = np.zeros((img.shape[:2]))
-    mask[10:125, 50:130] = 1
-    mask[10:125, 400:480] = 1"""
-    print(cv.__version__)
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier('Haarcascade_frontalface_default.xml')
+    img = cv2.imread("../../People.jpg")
+    print(type(img))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        img = cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,0), 2)
+
+    cv2.imwrite('mask1.jpg', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
     return blurring(img, 750, 0.24, mask)
 
 
@@ -33,7 +46,8 @@ class Anonymisering(md.Modifier):
 
 #   Testfunksjon. Slett ved endelig release
 if __name__ == "__main__":
-    img = imageio.imread('../../People.jpg')
+    img = np.array(imageio.imread('../../People.jpg'))
     new_img = anonymisering(img)
+    
     plt.imshow(new_img)
     plt.show()
